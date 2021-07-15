d8b727b75d182491a4dfcc87e8b0ab202306350d
Bodhisatwya Banerjee <clinitial@gmail.com>
2021-07-15 02:56:45 +0000

    restored java file

--- helloworld.java
+++ helloworld.java
@@ -1,15 +1,9 @@
 //This is a simple code to print for the end user
-//Added another print statement
 
 public class JavaHelloWorldProgram {
 
 	public static void main(String args[]){
 		System.out.println("Hello World");
-            
-               //  Prints "Hello World" in terminal window            
-
-		System.out.println("Welcome to Git");
-
 	}
 }
 
