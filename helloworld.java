611bb04e4bdeb2a4382524418feeef4051992080
Bodhisatwya Banerjee <clinitial@gmail.com>
2021-07-15 03:34:52 +0000

    added multiple print statements and comment line

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,11 @@
 //This is a simple code to print for the end user
+//Contains two print statements
 
 public class JavaHelloWorldProgram {
 
 	public static void main(String args[]){
 		System.out.println("Hello World");
+                System.out.println("Hello World");
 	}
 }
 
